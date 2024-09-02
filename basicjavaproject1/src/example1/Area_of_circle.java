0edd1d75a5a6cb8df313f8d1bfbdf1da4e5af899
gitAutomation123 <143370765+gitAutomation123@users.noreply.github.com>
2024-09-02 18:14:16 +0530

    Update Area_of_circle.java
    this program has been close

--- basicjavaproject1/src/example1/Area_of_circle.java
+++ basicjavaproject1/src/example1/Area_of_circle.java
@@ -18,6 +18,8 @@ public static void main(String[] args) {
      System.out.println("area of circle is= "+res);
      
      System.out.println("program ends");
+	 System.out.println("program lose");
+	
 	
 		
 		
